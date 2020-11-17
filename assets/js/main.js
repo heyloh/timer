a0967682bb316306d10ad6701cf118064c5abb69
Lohana Torres <lt.leopoldo@gmail.com>
2020-11-17 17:34:57 -0300

    ♻️ Refactor script structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,43 @@
-const clock = document.querySelector('.clock');
-const start = document.querySelector('.start');
-const pause = document.querySelector('.pause');
-const stop = document.querySelector('.stop');
-let seconds = 0;
-let timer;
+function timer() {
+  const clock = document.querySelector('.clock');
+  let seconds = 0;
+  let timer;
 
-start.addEventListener('click', (event) => {
-  clock.classList.remove('paused');
-  handleInitClock();
-});
+  document.addEventListener('click', (event) => {
+    const element = event.target;
 
-pause.addEventListener('click', (event) => {
-  clearInterval(timer);
-  clock.classList.add('paused');
-});
+    if (element.classList.contains('start')) {
+      clock.classList.remove('paused');
+      handleInitClock();
+    }
 
-stop.addEventListener('click', (event) => {
-  clearInterval(timer);
-  clock.classList.remove('paused');
-  clock.innerHTML = '00:00:00';
-  seconds = 0;
-});
+    if (element.classList.contains('pause')) {
+      clearInterval(timer);
+      clock.classList.add('paused');
+    }
 
-
-function handleReturnClockTime(seconds) {
-  const date = new Date(seconds * 1000);
-  return date.toLocaleTimeString('pt-BR', {
-    hour12: false,
-    timeZone: 'GMT'
+    if (element.classList.contains('stop')) {
+      clearInterval(timer);
+      clock.classList.remove('paused');
+      clock.innerHTML = '00:00:00';
+      seconds = 0;
+    }
   });
+
+  function handleReturnClockTime(seconds) {
+    const date = new Date(seconds * 1000);
+    return date.toLocaleTimeString('pt-BR', {
+      hour12: false,
+      timeZone: 'GMT'
+    });
+  }
+
+  function handleInitClock() {
+    timer = setInterval(function () {
+      seconds++;
+      clock.innerHTML = handleReturnClockTime(seconds);
+    }, 1000);
+  }
 }
 
-function handleInitClock() {
-  timer = setInterval(function () {
-    seconds++;
-    clock.innerHTML = handleReturnClockTime(seconds);
-  }, 1000);
-}
+timer();
