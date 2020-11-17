bbbc24674376acb32539abc0156904a5a3afc921
Lohana Torres <lt.leopoldo@gmail.com>
2020-11-17 17:21:42 -0300

    ✨ Add handleReturnClockTime function

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,13 @@ stop.addEventListener('click', (event) => {
   clearInterval(timer);
   clock.innerHTML = '00:00:00';
 });
+
+
+function handleReturnClockTime(seconds) {
+  const date = new Date(seconds * 1000);
+  return date.toLocaleTimeString('pt-BR', {
+    hour12: false,
+    timeZone: 'GMT'
+  });
+}
+
